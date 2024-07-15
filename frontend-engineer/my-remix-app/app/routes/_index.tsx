883222a23b0c111aa0606336c7cb8b9b51eda17b
Alexander Derritt <aderritt6158@gmail.com>
2024-07-14 19:20:27 -0500

    added kc-neighborhoods to loader

--- frontend-engineer/my-remix-app/app/routes/_index.tsx
+++ frontend-engineer/my-remix-app/app/routes/_index.tsx
@@ -6,14 +6,20 @@ import fs from 'fs'
 import path from 'path'
 
 export const loader = async () => {
-  const filePath = path.resolve('kc-tracts.json');
-  const fileContents = fs.readFileSync(filePath, 'utf8');
-  const geojsonData = JSON.parse(fileContents);
-  return json(geojsonData);
+  const tractsPath = path.resolve('kc-tracts.json');
+  const neighborhoodsPath = path.resolve('kc-neighborhoods.json')
+
+  const tractsContents = fs.readFileSync(tractsPath, 'utf8');
+  const neighborhoodsContents = fs.readFileSync(neighborhoodsPath, 'utf-8')
+
+  const tractsData = JSON.parse(tractsContents);
+  const neighborhoodsData = JSON.parse(neighborhoodsContents);
+
+  return json({ tractsData, neighborhoodsData });
 };
 
 export default function Index(){
-  const data = useLoaderData();
+  const { tractsData, neighborhoodsData } = useLoaderData<typeof loader>();
 
   useEffect(() => {
     mapboxgl.accessToken = 'pk.eyJ1IjoiYmxhc3Q2MSIsImEiOiJjbHltN21scnowdG95MmtwcnB6Yjd2dDJhIn0.PM7izJTLpphLWcN2VJvzPA' 
@@ -27,21 +33,37 @@ export default function Index(){
     map.on('load', () => {
       map.addSource('geojson-data', {
         'type': 'geojson',
-        'data': data
+        'data': tractsData
       });
 
       map.addLayer({
-        'id': 'geojson-layer',
+        'id': 'tracts-layer',
         'type': 'fill',
-        'source': 'geojson-data',
+        'source': 'tracts-data',
         'layout': {},
         'paint': {
           'fill-color': '#888888',
           'fill-opacity': 0.5
         }
       });
+      
+      map.addSource('neighborhoods-data', {
+        'type': 'geojson',
+        'data': neighborhoodsData
+      });
+      
+      map.addLayer({
+        'id': 'neighborhoods-layer',
+        'type': 'line',
+        'source': 'neighborhoods-data',
+        'layout': {},
+        'paint': {
+          'line-color': '#ff0000',
+          'line-width':2
+        }
+      });
     });
     return () => map.remove();
-  }, [data])
+  }, [tractsData, neighborhoodsData])
   return <div id='map' style={{ width: '100%', height: '500px' }}></div>
 }
